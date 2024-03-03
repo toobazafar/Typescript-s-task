36d29eeced594fabf2ab583d2fb644bdcc8717cb
toobazafar <160070906+toobazafar@users.noreply.github.com>
2024-03-04 00:27:57 +0500

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,2 +1,5 @@
-let message = "Hello World";
-console.log(message);
+//erercise 10
+//my name is TOOBA 
+//date: 1/3/2024
+//this program will run simple code just like hello world
+console.log(`hello world simple program`);
---
+++ program2.ts
@@ -0,0 +1,5 @@
+//erercise 10
+//my name is TOOBA 
+//date: 1/3/2024
+//this program wil multiplication 
+console.log(5*5);
